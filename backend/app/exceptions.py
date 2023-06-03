449932a45c1bf56360db370d3018498bb1d506f7
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2023-06-03 09:19:32 +0200

    update custome exceptions

---
+++ backend/app/exceptions.py
@@ -0,0 +1,32 @@
+class APIError(Exception):
+    pass
+
+
+class CannotStartNewGame(APIError):
+    code = 400
+    description = "Cannot start new game. Not enough credits."
+
+
+class CannotAddCredits(APIError):
+    code = 400
+    description = "Cannot add credits. Credits are not zero."
+
+
+class GameSessionNotFound(APIError):
+    code = 404
+    description = "Game session not found."
+
+
+class NotAllowedMove(APIError):
+    code = 400
+    description = "Not allowed move."
+
+
+class PlayerNotFound(APIError):
+    code = 404
+    description = "Player not found."
+
+
+class PlayerAlreadyExists(APIError):
+    code = 409
+    description = "Player already exists."
