b618b8fcbc5ab08fd0c40a20e015c305eb1f60e6
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2023-06-03 10:55:47 +0200

    add update session time by socketio

--- backend/app/socketio_handlers.py
+++ backend/app/socketio_handlers.py
@@ -1,7 +1,9 @@
-from .extensions import socketio
+from .extensions import socketio, db
+from .repositories import GameRepository
 
 
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
-    socketio.emit('message', data)
+@socketio.on('update_session_end_time')
+def update_end_time_handler(data):
+    game_repository = GameRepository(db.session)
+    game_session = game_repository.get_game_session(session_id=data['session_id'])
+    game_session.update_end_time()
